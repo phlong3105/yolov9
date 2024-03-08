6148abfa8364609747374b1306abd0c9d72d7c04
longpham <longpham3105@gmail.com>
2024-03-08 17:22:56 +0900

    Refactor

--- utils/general.py
+++ utils/general.py
@@ -515,7 +515,7 @@ def check_dataset(data, autodownload=True):
     # Resolve paths
     path = Path(extract_dir or data.get('path') or '')  # optional 'path' default to '.'
     if not path.is_absolute():
-        path = (ROOT / path).resolve()
+        path = DATASETS_DIR  # (ROOT / path).resolve()
         data['path'] = path  # download scripts
     for k in 'train', 'val', 'test':
         if data.get(k):  # prepend path
